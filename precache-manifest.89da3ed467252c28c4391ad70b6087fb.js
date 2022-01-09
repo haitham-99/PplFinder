c37311b925a9a20e4d6249bc3c97a38583d83bf1
haitham-99 <haithamodehodeh@gmail.com>
2022-01-09 15:09:42 +0200

    Updates

---
+++ precache-manifest.89da3ed467252c28c4391ad70b6087fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc219329a092c409ca99bb9cdce0669",
+    "url": "/PplFinder/index.html"
+  },
+  {
+    "revision": "96ff53af08f3ca3df875",
+    "url": "/PplFinder/static/js/2.aa8d6410.chunk.js"
+  },
+  {
+    "revision": "5735f43f0de66d591ec3",
+    "url": "/PplFinder/static/js/main.db21e9db.chunk.js"
+  },
+  {
+    "revision": "202fda6802749b2b2a88",
+    "url": "/PplFinder/static/js/runtime~main.1cb51378.js"
+  }
+]);
